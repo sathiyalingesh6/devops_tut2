01bca589d10645a63ed04c496de253ff801ea25f
sathiyalingesh6 <116438116+sathiyalingesh6@users.noreply.github.com>
2023-10-16 11:21:34 +0530

    Create sort2.py

---
+++ sort2.py
@@ -0,0 +1,14 @@
+# Input: A list of numbers
+numbers = [4, 2, 7, 1, 9, 5]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print("Sorted numbers in ascending order:", sorted_numbers)
+
+# Sort the list in descending order
+sorted_numbers_desc = sorted(numbers, reverse=True)
+
+# Print the sorted list in descending order
+print("Sorted numbers in descending order:", sorted_numbers_desc)
